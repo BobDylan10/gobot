26245029774b30b79084d9331300953a086dae72
Bob Dylan <poubelleab7@gmail.com>
2020-01-16 15:41:34 +0100

    players (suite)

--- players/database.go
+++ players/database.go
@@ -3,49 +3,50 @@ package players
 import (
 	"database/sql"
 	"testbot/database"
-	"fmt"
+	"time"
+	"testbot/log"
 )
 
-func getPlayerDid(guid string, name string) {
-	stmtOut, err := database.Database.Prepare("SELECT player_id, alias, last_connection, connections FROM players WHERE guid = ?")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+func getPlayer(guid string, name string, player *player){
+	stmtOut := database.Prepare("SELECT player_id, alias, level, last_connection, connections FROM players WHERE guid = ?")
 	defer stmtOut.Close()
 
 	var did int
 	var alias string
-	var date string
+	var level int
+	var date time.Time
 	var connections int
 
-	err = stmtOut.QueryRow(guid).Scan(&did, &alias, &date, &connections) // WHERE number = 13
+	err := stmtOut.QueryRow(guid).Scan(&did, &alias, &level, &date, &connections) // WHERE number = 13
 	if err != nil {
 		if (err == sql.ErrNoRows) {
-			fmt.Println("Creating player with GUID", guid)
+			log.Log(log.LOG_VERBOSE, "Creating player with GUID", guid)
 			createPlayer(guid, name)
 		} else {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
 	} else {
-		fmt.Println("Found player !")
+		log.Log(log.LOG_DEBUG, "Found player with guid", guid)
 	}
-	err = stmtOut.QueryRow(guid).Scan(&did, &alias, &date, &connections) // WHERE number = 13
+	err = stmtOut.QueryRow(guid).Scan(&did, &alias, &level, &date, &connections) // WHERE number = 13
 	if err != nil { //Here it should exist in all cases
 			panic(err.Error()) // proper error handling instead of panic in your app
 	}
-	fmt.Printf("alias is: %d, %s, %s, %d\n", did, alias, date, connections)
+	player.did = did
+	player.level = level
+	player.connections = connections
+	player.isBot = false
+	player.lastConnection = date
+	player.name = name
 }
 
 func createPlayer(guid string, name string) {
-	stmtIns, err := database.Database.Prepare("INSERT INTO players(alias, guid) VALUES( ?, ? )") // ? = placeholder
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+	stmtIns := database.Prepare("INSERT INTO players(alias, guid) VALUES( ?, ? )") // ? = placeholder
 	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
 
-	_, err = stmtIns.Exec(name, guid) // Insert tuples (i, i^2)
+	_, err := stmtIns.Exec(name, guid) // Insert tuples (i, i^2)
 		if err != nil {
-			fmt.Println("Error creating player", name, guid)
+			log.Log(log.LOG_ERROR, "Error creating player", name, guid)
 		}
 }
