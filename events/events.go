aa468de3d2685f2af3468157b3fc6b6c944ce31e
Bob Dylan <poubelleab7@gmail.com>
2020-01-20 16:55:39 +0100

    better formatting

--- events/events.go
+++ events/events.go
@@ -18,24 +18,24 @@ type Event interface{
 }
 
 type EventSay struct {
-	Client int
-	Text string
-}
-func (e EventSay) EventType() EventType {
-	return EVT_CLIENT_SAY
-}
+		Client int
+		Text string
+	}
+	func (e EventSay) EventType() EventType {
+		return EVT_CLIENT_SAY
+	}
 
 type EventClientInfo struct {
-	Client int
-	Data map[string] string
-}
-func (e EventClientInfo) EventType() EventType {
-	return EVT_CLIENT_INFO
-}
+		Client int
+		Data map[string] string
+	}
+	func (e EventClientInfo) EventType() EventType {
+		return EVT_CLIENT_INFO
+	}
 
 type EventClientDisconnect struct {
-	Client int
-}
-func (e EventClientDisconnect) EventType() EventType {
-	return EVT_CLIENT_DISCONNECT
-}
+		Client int
+	}
+	func (e EventClientDisconnect) EventType() EventType {
+		return EVT_CLIENT_DISCONNECT
+	}
