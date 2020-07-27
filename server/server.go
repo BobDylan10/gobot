420ff9bd240201e829e3359649bd93526c381a7d
Bob Dylan <poubelleab7@gmail.com>
2020-07-27 16:08:11 +0200

    Linter

--- server/server.go
+++ server/server.go
@@ -1,16 +1,16 @@
 package server
 
 import (
-	"testbot/server/rcon"
 	"regexp"
 	"strings"
 	"testbot/log"
+	"testbot/server/rcon"
 )
 
 var (
 	done = make(chan bool)
 	cmds = make(chan string)
-	aws = make(chan string)
+	aws  = make(chan string)
 )
 
 const lineLength = 80
@@ -23,34 +23,35 @@ func Close() {
 	<-done
 }
 
-func CallServer(command string) string{
+func CallServer(command string) string {
 	log.Log(log.LOG_DEBUG, "Executing command", command)
-	cmds<-command
+	cmds <- command
 	return <-aws
 }
+
 // num score ping guid   name            lastmsg address               qport rate
 // --- ----- ---- ------ --------------- ------- --------------------- ----- -----
 // 2     0   29 465030   ThorN                50 68.63.6.62:-32085      6597  5000
 const _regPlayer = `(?i)^(?P<slot>[0-9]+)\s+` +
-`(?P<score>[0-9-]+)\s+` +
-`(?P<ping>[0-9]+)\s+` +
-`(?P<guid>[0-9a-zA-Z]+)\s+` +
-`(?P<name>.*?)\s+` +
-`(?P<last>[0-9]+)\s+` +
-`(?P<ip>[0-9.]+):(?P<port>[0-9-]+)\s+` +
-`(?P<qport>[0-9]+)` +
-`\s+(?P<rate>[0-9]+)$`
+	`(?P<score>[0-9-]+)\s+` +
+	`(?P<ping>[0-9]+)\s+` +
+	`(?P<guid>[0-9a-zA-Z]+)\s+` +
+	`(?P<name>.*?)\s+` +
+	`(?P<last>[0-9]+)\s+` +
+	`(?P<ip>[0-9.]+):(?P<port>[0-9-]+)\s+` +
+	`(?P<qport>[0-9]+)` +
+	`\s+(?P<rate>[0-9]+)$`
 
 func getParams(regEx *regexp.Regexp, url string) (paramsMap map[string]string) {
 	match := regEx.FindStringSubmatch(url)
 
-    paramsMap = make(map[string]string)
-    for i, name := range regEx.SubexpNames() {
-        if i > 0 && i <= len(match) {
-            paramsMap[name] = match[i]
-        }
-    }
-    return
+	paramsMap = make(map[string]string)
+	for i, name := range regEx.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			paramsMap[name] = match[i]
+		}
+	}
+	return
 }
 
 func getPlayers() { //Quickfix for Urt, should be done inside rcon
@@ -58,24 +59,24 @@ func getPlayers() { //Quickfix for Urt, should be done inside rcon
 	lines := strings.Split(res, "\n")
 	reg := regexp.MustCompile(_regPlayer)
 	for i, line := range lines {
-		if (i >= 3) {
-			if (reg.MatchString(line)) {
+		if i >= 3 {
+			if reg.MatchString(line) {
 				getParams(reg, line)
 			}
 		}
 	}
 }
 
-func wrap(longstring string) []string{
+func wrap(longstring string) []string {
 	res := make([]string, 0)
-	parts := len(longstring)/lineLength
+	parts := len(longstring) / lineLength
 	i := 0
 	for i = 0; i < parts; i++ {
-		res = append(res, longstring[lineLength * i: lineLength * (i + 1)])
+		res = append(res, longstring[lineLength*i:lineLength*(i+1)])
 	}
 	rest := len(longstring) % lineLength
 	if rest != 0 {
-		res = append(res, longstring[lineLength * i: lineLength * i + rest])
+		res = append(res, longstring[lineLength*i:lineLength*i+rest])
 	}
 	return res
 }
